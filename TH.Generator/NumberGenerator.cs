69a8291f1a6a261caf300b2c5cb77fbc9575089c
Thanh Tran Duy - Dev <thanhtd@devblock.net>
2024-10-23 23:22:54 +0700

    build number generator

---
+++ TH.Generator/NumberGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TH.Generator
+{
+    public static class NumberGenerator
+    {
+        public static int[] GetRandomNumbers(int length, int min, int max)
+        {
+            int[] randomNumbers = new int[length];
+            var random = new Random();
+            
+            unsafe
+            {
+                fixed (int* p = randomNumbers)
+                {
+                    int* current = p;
+                    for (int i = 0; i < length; i++)
+                    {
+                        *current = random.Next(min, max + 1);
+                        current++;
+                    }
+                }
+            }
+
+            return randomNumbers;
+        }
+    }
+}
